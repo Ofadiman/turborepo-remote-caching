0aa0769dd26d781c80026857e7202e295a6a2f63
ofadiman <ofadiman@gmail.com>
2023-03-13 22:30:52 +0100

    feature: header component

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Button } from "ui";
+import { Button, Header } from "ui";
 
 export default function Docs() {
   return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
+    <>
+      <Header />
+      <div>
+        <h1>Docs</h1>
+        <Button />
+      </div>
+    </>
   );
 }
