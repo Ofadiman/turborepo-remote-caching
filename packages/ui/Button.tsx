dcaae272412a0e3f63c15614b8150f274ada3ccb
ofadiman <ofadiman@gmail.com>
2023-03-13 22:10:19 +0100

    feature: make counter button

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,14 @@
-import * as React from "react";
+import { useState } from "react";
+
 export const Button = () => {
-  return <button>Boop</button>;
+  const [count, setCount] = useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCount((prevState) => prevState + 1);
+      }}
+    >
+      Booped {count} times
+    </button>
+  );
 };
